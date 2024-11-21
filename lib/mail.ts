48d417a0aedad07dfc733a9c4dd479cc79195d3b
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2024-11-21 21:36:15 +0700

    Fixing Resend 3

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,22 +1,24 @@
-import { Resend } from "resend"
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY) || 're_PiJuNRej_C1eSyxFvgN1QqSFgqS6HKfak';
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const domain = process.env.NEXT_PUBLIC_API_URL
+const domain = process.env.NEXT_PUBLIC_API_URL || "https://www.wheelbox.tech";
 
 export const sendEmailNotification = async (email: string) => {
-    try {
-        const response = await resend.emails.send({
-            from: "dev.wheelbox@gmail.com",
-            to: email,
-            subject: "Thank You For Join With Us!",
-            html: `
-            <img src="${domain}/Assets/Images/Thankyou.png">
-            `
-        });
-        console.log("Email sent successfully:", response);
-    } catch (error) {
-        console.error("Failed to send email:", error);
-        throw new Error("Email sending failed");
+  try {
+    console.log("Sending email to:", email);
+    const response = await resend.emails.send({
+      from: "dev.wheelbox@gmail.com",
+      to: email,
+      subject: "Thank You For Join With Us!",
+      html: `<img src="${domain}/Assets/Images/Thankyou.png">`
+    });
+    console.log("Email sent successfully:", response);
+  } catch (error: any) {
+    console.error("Failed to send email:", error);
+    if (error.response) {
+      console.error("Resend API response:", error.response.data);
     }
+    throw new Error("Email sending failed");
+  }
 };
