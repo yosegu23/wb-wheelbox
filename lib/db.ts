086de9c91209d6be7a5186ce992c060d3d4ddc1c
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2024-11-21 20:34:17 +0700

    Second Commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,13 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined
+    var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma;
+}
+
+export const db = prisma;
