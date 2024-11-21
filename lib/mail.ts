ebb370bead68f23e251a64d448c7584295dda3eb
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2024-11-21 21:24:54 +0700

    Fixing Resend Email

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY) || 're_DmZQ2UoS_F5jLKwch89GgFnFXW2xmB4Fu';
 
 const domain = process.env.NEXT_PUBLIC_API_URL
 
