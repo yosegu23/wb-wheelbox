d88d731f015fd47316861bcb26572847905504c9
Yosegu23 <yopi.septian23@gmail.com>
2025-05-23 01:34:29 +0700

    Adding New Team Pages - 25 May 2025

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import { TeamWb } from "../components/team-wb";
@@ -15,6 +15,7 @@ import hendra from "@/public/Team/10.png";
 import elvio from "@/public/Team/11.png";
 import ridwan from "@/public/Team/12.png";
 import hafidz from "@/public/Team/13.png";
+import Footer from "../components/footer";
 
 const teamMembers = [
   {
@@ -86,19 +87,23 @@ const teamMembers = [
 
 const Teams = () => {
   return (
-    <div className="w-full h-full flex flex-col items-start justify-start lg:p-[170px] gap-8">
-      <span className="text-xs h-auto">Our Team</span>
-      <div className="flex flex-col items-start justify-start gap-2">
-        <h1 className="text-slate-500 text-5xl">Meet Our Team</h1>
-        <h1 className="text-5xl">Passionate. Innovation. Expert</h1>
-      </div>
-      <p className="lg:w-[450px] text-base leading-relaxed">
-        We lead with care-our core value-and a share passion for connection the
-        world.
-      </p>
+    <>
+      <div className="w-full h-full flex flex-col items-start justify-start lg:p-[170px] gap-8">
+        <span className="text-xs h-auto underline-offset-1">Our Team</span>
+        <div className="flex flex-col items-start justify-start gap-2">
+          <h1 className="text-slate-500 text-5xl">Meet Our Team</h1>
+          <h1 className="text-5xl">Passionate. Innovation. Expert</h1>
+        </div>
+        <p className="lg:w-[450px] text-base leading-relaxed">
+          We lead with care-our core value-and a share passion for connection
+          the world.
+        </p>
 
-      <TeamWb members={teamMembers} />
-    </div>
+        <TeamWb members={teamMembers} />
+
+        <Footer />
+      </div>
+    </>
   );
 };
 
