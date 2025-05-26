8833b1216e502461aaacecc4fe6b5fe600e5ff32
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2025-05-26 11:32:52 +0700

    Adding Core Values Pages - 26 May 2025

---
+++ app/core-values/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { HeroHeader } from "./hero-header-cv";
+
+export const metadata: Metadata = {
+  title: "WheelBox |Team Of Wheelbox",
+  description:
+    "Startup Cianjur - WheelBox, a startup based in Cianjur, Indonesia, focusing on innovation and technology.",
+  keywords: [
+    "Startup Cianjur",
+    "WheelBox",
+    "Startup WheelBox",
+    "WheelBox Startup",
+    "Startup Indonesia",
+  ],
+};
+
+export default function CoreValuesLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <HeroHeader />
+      <div className="w-full h-full flex justify-center items-center">
+        {children}
+      </div>
+    </>
+  );
+}
---
+++ app/core-values/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CoreValuePage = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-start justify-start lg:p-[170px] gap-8">
+      Core Value Page
+    </div>
+  );
+};
+
+export default CoreValuePage;
