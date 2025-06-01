3582668545f3a3a3f3fbe6ad98962524a41e4dad
Yosegu23 <yopi.septian23@gmail.com>
2025-06-01 18:13:43 +0700

    Fixing Hero Section Scroll Down Smartphone - 01 June 2025

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -38,16 +38,16 @@ export default function HeroSection() {
   return (
     <>
       <HeroHeader />
-      <main className="overflow-x-hidden">
+      <main className="overflow-x-hidden min-h-screen overflow-y-auto">
         <div
           aria-hidden
-          className="absolute inset-0 pointer-events-none isolate hidden opacity-65 contain-strict lg:block">
+          className="absolute inset-0 lg:pointer-events-none lg:isolate hidden opacity-65 contain-strict lg:block">
           <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
           <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
           <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
         </div>
         <section>
-          <div className="absolute inset-0">
+          <div className="absolute inset-0 pointer-events-none">
             <Waves
               lineColor={
                 theme === "dark"
